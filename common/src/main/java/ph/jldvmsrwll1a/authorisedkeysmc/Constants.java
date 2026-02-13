bb630d0b64785f8eba665113a09f51ccdc286e36
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2026-02-13 13:49:18 -0600

    Change LOGIN_CHANNEL_ID.

--- common/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/Constants.java
+++ common/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/Constants.java
@@ -9,10 +9,14 @@ public class Constants {
     public static final String MOD_DIR_NAME = "authorised_keys_mc";
     public static final String MOD_NAME = "AuthorisedKeysMC";
 
-    public static final Identifier LOGIN_CHANNEL_ID = Identifier.fromNamespaceAndPath("authorised_keys_mc", "login");
+    public static final Identifier LOGIN_CHANNEL_ID = modId("login");
 
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
     public static final int PAYLOAD_HEADER = 0x414B4D43;
     public static final int PROTOCOL_VERSION = 100;
+
+    public static Identifier modId(String path) {
+        return Identifier.fromNamespaceAndPath(MOD_ID, path);
+    }
 }
