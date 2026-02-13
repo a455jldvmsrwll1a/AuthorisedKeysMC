afb9aff7f06be6d87ba1d37c97f8c593063674f3
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2026-02-13 14:31:55 -0600

    Properly register onTick() on Forge.

--- forge/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/AuthorisedKeysMC.java
+++ forge/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/AuthorisedKeysMC.java
@@ -1,8 +1,6 @@
 package ph.jldvmsrwll1a.authorisedkeysmc;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLLoader;
 import ph.jldvmsrwll1a.authorisedkeysmc.platform.ForgePlatformHelper;
@@ -10,7 +8,7 @@ import ph.jldvmsrwll1a.authorisedkeysmc.platform.ForgePlatformHelper;
 @Mod(Constants.MOD_ID)
 public class AuthorisedKeysMC {
     public AuthorisedKeysMC() {
-        MinecraftForge.EVENT_BUS.register(this);
+        TickEvent.ClientTickEvent.Post.BUS.addListener(this::onTick);
 
         AuthorisedKeysModCore.init(new ForgePlatformHelper());
 
@@ -19,7 +17,6 @@ public class AuthorisedKeysMC {
         }
     }
 
-    @SubscribeEvent
     private void onTick(TickEvent.ClientTickEvent.Post event) {
         AuthorisedKeysModClient.tick();
     }
