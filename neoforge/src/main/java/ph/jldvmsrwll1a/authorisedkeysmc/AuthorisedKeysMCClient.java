45678d61e42d5aadc57f1880f4a8a33d419d40df
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2026-02-13 14:49:02 -0600

    Register portal screen as config screen for NeoForge.

--- neoforge/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/AuthorisedKeysMCClient.java
+++ neoforge/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/AuthorisedKeysMCClient.java
@@ -1,16 +1,21 @@
 package ph.jldvmsrwll1a.authorisedkeysmc;
 
+import java.util.function.Supplier;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
+import ph.jldvmsrwll1a.authorisedkeysmc.gui.PortalScreen;
 
 @Mod(value = Constants.MOD_ID, dist = Dist.CLIENT)
 public class AuthorisedKeysMCClient {
 
-    public AuthorisedKeysMCClient(IEventBus modBus) {
+    public AuthorisedKeysMCClient(ModContainer container) {
         NeoForge.EVENT_BUS.addListener(AuthorisedKeysMCClient::onTick);
+        container.registerExtensionPoint(IConfigScreenFactory.class, (Supplier<IConfigScreenFactory>)
+                () -> (minecraft, parent) -> new PortalScreen(parent));
 
         AuthorisedKeysModClient.init();
     }
