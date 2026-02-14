bc90873af3e4038c59b218fd37b1d3a76f4a79a7
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2026-02-14 17:13:03 -0600

    Pick saner KDF parameters.

--- common/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/crypto/AkEncryptedKey.java
+++ common/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/crypto/AkEncryptedKey.java
@@ -12,6 +12,7 @@ import java.nio.ByteBuffer;
 import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.Random;
 
 public class AkEncryptedKey {
     private static final int KDF_SALT_LENGTH = 32;
@@ -22,28 +23,30 @@ public class AkEncryptedKey {
     private static final int PLAINTEXT_LENGTH = AkPrivateKey.LENGTH;
     private static final int OUTPUT_LENGTH = PLAINTEXT_LENGTH + MAC_LENGTH;
 
-    private final int iterations;
-    private final int memory;
-    private final int parallelism;
+    private static final int DEFAULT_TARGET_MS = 900;
+
+    private final Parameters parameters;
     private final byte[] salt;
     private final byte[] nonce;
     private final byte[] cipherText;
 
     public AkEncryptedKey(SecureRandom random, AkPrivateKey privateKey, char[] password) {
+        this(random, privateKey, Parameters.calibrate(DEFAULT_TARGET_MS), password);
+    }
+
+    public AkEncryptedKey(SecureRandom random, AkPrivateKey privateKey, Parameters parameters, char[] password) {
+        this.parameters = parameters;
+
         byte[] key = null;
 
         try {
-            memory = pickMemoryParameter();
-            parallelism = pickParallelismParameter();
-            iterations = pickIterationParameter(memory, parallelism);
-
             salt = new byte[KDF_SALT_LENGTH];
             nonce = new byte[NONCE_LENGTH];
 
             random.nextBytes(salt);
             random.nextBytes(nonce);
 
-            key = deriveKey(password, salt, iterations, memory, parallelism);
+            key = deriveKey(password, salt, parameters.iterations, parameters.memory, parameters.parallelism);
             cipherText = encipher(key, nonce, privateKey.getInternal().getEncoded());
         } finally {
             if (key != null) {
@@ -57,9 +60,11 @@ public class AkEncryptedKey {
         nonce = new byte[NONCE_LENGTH];
         cipherText = new byte[OUTPUT_LENGTH];
 
-        iterations = buffer.get();
-        parallelism = buffer.get();
-        memory = buffer.getShort();
+        byte iterations = buffer.get();
+        byte parallelism = buffer.get();
+        short memoryMiB = buffer.getShort();
+
+        parameters = new Parameters(iterations, memoryMiB * 1024, parallelism);
 
         buffer.get(salt);
         buffer.get(nonce);
@@ -67,9 +72,11 @@ public class AkEncryptedKey {
     }
 
     public void write(ByteBuffer buffer) {
-        buffer.put((byte) iterations);
-        buffer.put((byte) parallelism);
-        buffer.putShort((short) memory);
+        short memoryMiB = (short) (parameters.memory / 1024);
+
+        buffer.put((byte) parameters.iterations);
+        buffer.put((byte) parameters.parallelism);
+        buffer.putShort(memoryMiB);
 
         buffer.put(salt);
         buffer.put(nonce);
@@ -80,7 +87,8 @@ public class AkEncryptedKey {
         byte[] key = null;
 
         try {
-            key = deriveKey(password, salt, iterations, memory, parallelism);
+            key = deriveKey(password, salt, parameters.iterations, parameters.memory, parameters.parallelism);
+
             byte[] decryptedBytes = decipher(key);
 
             return Optional.of(new AkPrivateKey(decryptedBytes));
@@ -93,26 +101,27 @@ public class AkEncryptedKey {
         }
     }
 
-    private static byte[] deriveKey(char[] password, byte[] salt, int iterations, int memory, int parallelism) {
-        Validate.isTrue(salt.length == KDF_SALT_LENGTH, "Wrong salt length.");
-
-        int memoryKiB = memory * 1024;
-
+    private static byte[] deriveKey(char[] password, byte[] salt, int t, int m, int p) {
         byte[] key = new byte[KDF_HASH_LENGTH];
+        deriveKey(password, salt, t, m, p, key);
+        return key;
+    }
 
-        Argon2Parameters parameters = new Argon2Parameters.Builder(Argon2Parameters.ARGON2_id)
+    private static void deriveKey(char[] password, byte[] salt, int t, int m, int p, byte[] out) {
+        Validate.isTrue(salt.length == KDF_SALT_LENGTH, "Wrong salt length.");
+        Validate.isTrue(out.length == KDF_HASH_LENGTH, "Wrong output buffer length.");
+
+        Argon2Parameters argon2Parameters = new Argon2Parameters.Builder(Argon2Parameters.ARGON2_id)
                 .withVersion(Argon2Parameters.ARGON2_VERSION_13)
-                .withIterations(iterations)
-                .withMemoryAsKB(memoryKiB)
-                .withParallelism(parallelism)
+                .withIterations(t)
+                .withMemoryAsKB(m)
+                .withParallelism(p)
                 .withSalt(salt)
                 .build();
 
         Argon2BytesGenerator generator = new Argon2BytesGenerator();
-        generator.init(parameters);
-        generator.generateBytes(password, key);
-
-        return key;
+        generator.init(argon2Parameters);
+        generator.generateBytes(password, out);
     }
 
     private byte[] decipher(byte[] key) throws InvalidCipherTextException {
@@ -165,31 +174,91 @@ public class AkEncryptedKey {
         return cipherText;
     }
 
-    private static int pickIterationParameter(int memory, int parallelism) {
-        int i = 8;
+    public static final class Parameters {
+        private static final char[] PASSWORD = "balls oyster 123".toCharArray();
 
-        if (parallelism > 4) {
-            i *= 2;
+        private final int iterations;
+        private final int memory;
+        private final int parallelism;
+
+        public Parameters(int t, int m, int p) {
+            iterations = t;
+            memory = m;
+            parallelism = p;
         }
 
-        i /= memory / 128;
+        public static Parameters calibrate(int targetMillis) {
+            byte[] salt = generateInsecureSalt();
+            byte[] output = new byte[KDF_HASH_LENGTH];
 
-        return Math.clamp(i, 2, 16);
-    }
+            int memory = pickMemoryParameter();
+            int parallelism = pickParallelismParameter();
 
-    private static int pickMemoryParameter() {
-        Runtime runtime = Runtime.getRuntime();
+            // Warm-up.
+            deriveKey(PASSWORD, salt, 1, memory, parallelism, output);
+            deriveKey(PASSWORD, salt, 1, memory, parallelism, output);
 
-        long allocated = runtime.totalMemory() - runtime.freeMemory();
-        long available = runtime.maxMemory() - allocated;
-        long mib = available / 1048576;
+            // Measure time taken for t = 1, 2, and 3.
 
-        return Math.clamp(mib, 32, 512);
-    }
+            long start = System.nanoTime();
+            deriveKey(PASSWORD, salt, 1, memory, parallelism, output);
+            long end = System.nanoTime();
+            double t1 = (double) (end - start) / 1000.0;
 
-    private static int pickParallelismParameter() {
-        int cores = Runtime.getRuntime().availableProcessors();
+            start = System.nanoTime();
+            deriveKey(PASSWORD, salt, 2, memory, parallelism, output);
+            end = System.nanoTime();
+            double t2 = (double) (end - start) / 1000.0;
 
-        return Math.clamp(cores / 3, 1, 8);
+            start = System.nanoTime();
+            deriveKey(PASSWORD, salt, 3, memory, parallelism, output);
+            end = System.nanoTime();
+            double t3 = (double) (end - start) / 1000.0;
+
+            // Linear regression for 3 points via least squares method.
+
+            double xs = 6.0;
+            double ys = t1 + t2 + t3;
+            double xys = t1 + (2.0 * t2) + (3.0 * t3);
+            double xxs = 14.0;
+
+            // Find the linear equation.
+            double slope = (3.0 * xys - xs * ys) / (3.0 * xxs - xs * xs);
+            double intercept = (ys - slope * xs) / 3.0;
+
+            double target = (double) targetMillis * 1000.0;
+            // Solve for predicted t parameter with the inverse of the equation.
+            double predicted = (target - intercept) / slope;
+
+            int iterations = (int) Math.ceil(Math.clamp(predicted, 1.0, 32.0));
+
+            Arrays.fill(output, (byte) 0);
+            return new Parameters(iterations, memory, parallelism);
+        }
+
+        private static int pickMemoryParameter() {
+            Runtime runtime = Runtime.getRuntime();
+
+            long allocated = runtime.totalMemory() - runtime.freeMemory();
+            long available = runtime.maxMemory() - allocated;
+            long mib = available / 1048576;
+
+            return Math.clamp(mib / 4, 64, 512) * 1024;
+        }
+
+        private static int pickParallelismParameter() {
+            int cores = Runtime.getRuntime().availableProcessors();
+
+            return Math.clamp(cores / 3, 1, 16);
+        }
+
+        private static byte[] generateInsecureSalt() {
+            byte[] salt = new byte[KDF_SALT_LENGTH];
+
+            Random random = new Random();
+            random.nextBytes(salt);
+
+            return salt;
+        }
     }
 }
