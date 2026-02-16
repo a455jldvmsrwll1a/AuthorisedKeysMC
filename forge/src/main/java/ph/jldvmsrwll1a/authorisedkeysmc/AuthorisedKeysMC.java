2b882f08437cbb46a8f55fc7e4c935e33390f214
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2026-02-15 20:32:56 -0600

    Use config button in Forge mod menu.

--- forge/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/AuthorisedKeysMC.java
+++ forge/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/AuthorisedKeysMC.java
@@ -1,21 +1,28 @@
 package ph.jldvmsrwll1a.authorisedkeysmc;
 
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
 import ph.jldvmsrwll1a.authorisedkeysmc.command.ModCommands;
+import ph.jldvmsrwll1a.authorisedkeysmc.gui.PortalScreen;
 import ph.jldvmsrwll1a.authorisedkeysmc.platform.ForgePlatformHelper;
 
 @Mod(Constants.MOD_ID)
 public class AuthorisedKeysMC {
-    public AuthorisedKeysMC() {
+    public AuthorisedKeysMC(FMLJavaModLoadingContext context) {
         TickEvent.ClientTickEvent.Post.BUS.addListener(this::onTick);
         RegisterCommandsEvent.BUS.addListener(this::onCommandRegistration);
 
         AuthorisedKeysModCore.init(new ForgePlatformHelper());
 
         if (FMLLoader.getDist().isClient()) {
+            context.registerExtensionPoint(
+                    ConfigScreenHandler.ConfigScreenFactory.class,
+                    () -> new ConfigScreenHandler.ConfigScreenFactory((minecraft, parent) -> new PortalScreen(parent)));
+
             AuthorisedKeysModClient.init();
         }
     }
