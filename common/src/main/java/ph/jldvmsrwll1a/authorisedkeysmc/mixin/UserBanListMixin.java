a4a74406e73d062c935b38edd1ad6c5d199ee3a7
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2026-02-23 02:00:20 -0600

    Override player lists to use profile names instead of IDs (configurable).

---
+++ common/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/mixin/UserBanListMixin.java
@@ -0,0 +1,21 @@
+package ph.jldvmsrwll1a.authorisedkeysmc.mixin;
+
+import net.minecraft.server.players.NameAndId;
+import net.minecraft.server.players.UserBanList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import ph.jldvmsrwll1a.authorisedkeysmc.AkmcCore;
+
+// Based on https://github.com/NikitaCartes/EasyWhitelist
+
+@Mixin(UserBanList.class)
+public class UserBanListMixin {
+    @Inject(method = "getKeyForUser(Lnet/minecraft/server/players/NameAndId;)Ljava/lang/String;", at = @At("HEAD"), cancellable = true)
+    private void useNameAsKey(NameAndId profile, CallbackInfoReturnable<String> cir) {
+        if (AkmcCore.CONFIG.matchPlayerListByName) {
+            cir.setReturnValue(profile.name());
+        }
+    }
+}
