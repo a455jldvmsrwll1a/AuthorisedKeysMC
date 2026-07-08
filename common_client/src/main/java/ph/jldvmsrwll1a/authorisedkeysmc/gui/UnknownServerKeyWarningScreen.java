65d7d6e0328d2d012ec34e19ec4a8345c61fae6c
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2026-07-08 12:52:20 -0500

    Improve server key warning screens.

--- common_client/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/gui/UnknownServerKeyWarningScreen.java
+++ common_client/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/gui/UnknownServerKeyWarningScreen.java
@@ -2,16 +2,25 @@ package ph.jldvmsrwll1a.authorisedkeysmc.gui;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.*;
 import org.apache.commons.lang3.function.BooleanConsumer;
 import ph.jldvmsrwll1a.authorisedkeysmc.crypto.AkPublicKey;
 import ph.jldvmsrwll1a.authorisedkeysmc.net.ClientLoginHandler;
-import ph.jldvmsrwll1a.authorisedkeysmc.util.Base64Util;
 
 public class UnknownServerKeyWarningScreen extends SimpleYesNoCancelScreen {
     private static final Component TITLE = Component.translatable("authorisedkeysmc.screen.unknown-server-key.title")
             .withStyle(ChatFormatting.BOLD)
             .withStyle(ChatFormatting.GOLD);
+    private static final Component PROMPT1 =
+            Component.translatable("authorisedkeysmc.screen.unknown-server-key.prompt1");
+    private static final Component PROMPT2 =
+            Component.translatable("authorisedkeysmc.screen.unknown-server-key.prompt2");
+    private static final Component PROMPT3 = Component.translatable(
+                    "authorisedkeysmc.screen.unknown-server-key.prompt3")
+            .withStyle(ChatFormatting.GOLD);
+    private static final Component PROMPT4 =
+            Component.translatable("authorisedkeysmc.screen.unknown-server-key.prompt4");
+    private static final Component COPY_LABEL = Component.translatable("authorisedkeysmc.tooltip.copy");
 
     public UnknownServerKeyWarningScreen(Screen parent, Component prompt, BooleanConsumer action) {
         super(parent, TITLE, prompt, action, null);
@@ -21,8 +30,21 @@ public class UnknownServerKeyWarningScreen extends SimpleYesNoCancelScreen {
             ClientLoginHandler context, AkPublicKey serverKey, BooleanConsumer action) {
         Screen parent = context.getMinecraft().screen;
         String name = context.getServerName().orElse("<no name>");
-        String keyStr = Base64Util.encode(serverKey.getEncoded());
-        Component prompt = Component.translatable("authorisedkeysmc.screen.unknown-server-key.prompt", name, keyStr);
+        String keyStr = serverKey.toString();
+
+        MutableComponent prompt = PROMPT1.copy();
+        prompt.append(Component.literal(name).withStyle(ChatFormatting.GRAY));
+        prompt.append(PROMPT2);
+        prompt.append("   ");
+        prompt.append(Component.literal(keyStr)
+                .withStyle(Style.EMPTY
+                        .withColor(ChatFormatting.AQUA)
+                        .withHoverEvent(new HoverEvent.ShowText(COPY_LABEL))
+                        .withClickEvent(new ClickEvent.CopyToClipboard(keyStr))));
+        prompt.append("\n\n");
+        prompt.append(PROMPT3);
+        prompt.append("\n\n");
+        prompt.append(PROMPT4);
 
         return new UnknownServerKeyWarningScreen(parent, prompt, action);
     }
--- common_client/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/gui/WrongServerKeyWarningScreen.java
+++ common_client/src/main/java/ph/jldvmsrwll1a/authorisedkeysmc/gui/WrongServerKeyWarningScreen.java
@@ -2,7 +2,7 @@ package ph.jldvmsrwll1a.authorisedkeysmc.gui;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.*;
 import org.apache.commons.lang3.function.BooleanConsumer;
 import ph.jldvmsrwll1a.authorisedkeysmc.crypto.AkPublicKey;
 import ph.jldvmsrwll1a.authorisedkeysmc.net.ClientLoginHandler;
@@ -10,7 +10,15 @@ import ph.jldvmsrwll1a.authorisedkeysmc.net.ClientLoginHandler;
 public final class WrongServerKeyWarningScreen extends SimpleYesNoCancelScreen {
     private static final Component TITLE = Component.translatable("authorisedkeysmc.screen.wrong-server-key.title")
             .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.RED);
+            .withStyle(ChatFormatting.DARK_RED);
+    private static final Component PROMPT1 = Component.translatable("authorisedkeysmc.screen.wrong-server-key.prompt1");
+    private static final Component PROMPT2 = Component.translatable("authorisedkeysmc.screen.wrong-server-key.prompt2");
+    private static final Component PROMPT3 = Component.translatable("authorisedkeysmc.screen.wrong-server-key.prompt3");
+    private static final Component PROMPT4 = Component.translatable("authorisedkeysmc.screen.wrong-server-key.prompt4");
+    private static final Component PROMPT5 = Component.translatable("authorisedkeysmc.screen.wrong-server-key.prompt5")
+            .withStyle(ChatFormatting.GOLD);
+    private static final Component PROMPT6 = Component.translatable("authorisedkeysmc.screen.wrong-server-key.prompt6");
+    private static final Component COPY_LABEL = Component.translatable("authorisedkeysmc.tooltip.copy");
 
     public WrongServerKeyWarningScreen(Screen parent, Component prompt, BooleanConsumer action) {
         super(parent, TITLE, prompt, action, null);
@@ -19,9 +27,29 @@ public final class WrongServerKeyWarningScreen extends SimpleYesNoCancelScreen {
     public static WrongServerKeyWarningScreen create(
             ClientLoginHandler context, AkPublicKey cachedKey, AkPublicKey currentKey, BooleanConsumer action) {
         Screen parent = context.getMinecraft().screen;
+
+        String cachedStr = cachedKey.toString();
+        String currentStr = currentKey.toString();
         String name = context.getServerName().orElse("<no name>");
-        Component prompt = Component.translatable(
-                "authorisedkeysmc.screen.wrong-server-key.prompt", name, cachedKey.toString(), currentKey.toString());
+
+        MutableComponent prompt = PROMPT1.copy();
+        prompt.append(Component.literal(name).withStyle(ChatFormatting.GRAY));
+        prompt.append(PROMPT2);
+        prompt.append("   ");
+        prompt.append(Component.literal(cachedStr)
+                .withStyle(Style.EMPTY
+                        .withColor(ChatFormatting.AQUA)
+                        .withHoverEvent(new HoverEvent.ShowText(COPY_LABEL))
+                        .withClickEvent(new ClickEvent.CopyToClipboard(cachedStr))));
+        prompt.append(PROMPT3);
+        prompt.append(Component.literal(currentStr)
+                .withStyle(Style.EMPTY
+                        .withColor(ChatFormatting.RED)
+                        .withHoverEvent(new HoverEvent.ShowText(COPY_LABEL))
+                        .withClickEvent(new ClickEvent.CopyToClipboard(currentStr))));
+        prompt.append(PROMPT4);
+        prompt.append(PROMPT5);
+        prompt.append(PROMPT6);
 
         return new WrongServerKeyWarningScreen(parent, prompt, action);
     }
